2d977ec51da8cb1ce32d74e9c59d8d4e69cfc186
Jyoti Bembde <jyoti@penieltech.com>
2023-06-22 14:09:33 +0530

    Employee details report in new module

---
+++ employee_report/employee_report/report/employee_details/employee_details.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, Peniel Technology LLC and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Employee Details"] = {
+	"filters": [
+
+	]
+};
